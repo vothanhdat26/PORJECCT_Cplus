63e4c88cb19b6a62d858d8c140456613960f83e5
vothanhdat26 <vothanhdat26012001@gmail.com>
2023-09-15 23:32:51 +0700

    addfuntion_nofunction_name_num

--- ATM_MANAGEMENT/main.cpp
+++ ATM_MANAGEMENT/main.cpp
@@ -10,7 +10,7 @@ private:
     int ac_Numer;
     double balance;
 public:
-    ATM(string ac_name, int ac_Numer,double balance){
+    ATM(double balance){
         this->ac_name= ac_name;
         this->ac_Numer = ac_Numer;
         this->balance = balance;
@@ -35,11 +35,13 @@ public:
         if(amount > balance){
             return false;
         }
-        balance = balance - amount;
+        balance -=amount;
+        // balance = balance - amount;
         return true;
     }
     void depost(double amount){
-        balance = balance + amount;
+        // balance = balance + amount;
+        balance +=amount;
     }
     void display(){
         cout <<"Account Name: "<< ac_name << endl;
@@ -49,11 +51,13 @@ public:
 };
 int main()
 {
-    ATM atm("",14,15);
+    ATM atm(1000);
     char choice;
+    int op;
+    double amount,success;
     do{
 		system("cls");
-		int op;
+
 		cout << "----------------------------------------------------------------------------" << endl;
 		cout << "\t\t\t ATM Management System" << endl;
 		cout << "----------------------------------------------------------------------------" << endl;
@@ -67,13 +71,39 @@ int main()
         switch (op)
         {
         case 1:
-        
+        // so du cua ban
+            cout <<"Your Balance: " << atm.getBalance();
             break;
-        
+        case 2: 
+        // nhap so tien can rut
+            cout <<"Enter the Amount to Withdraw " ;
+            cin >>amount;
+            success = atm.withdraw(amount);
+            if(success){
+                // rut tien thanh cong
+                cout << "withdraw Successf ully.....!" << endl;
+            }   
+            else{
+                // so du khong du
+                cout <<"Insufficient Balance..... " << endl;
+            }
+            break;
+        case 3:
+        // nhap so tien can gui
+            cout <<"Enter the Amount to Deposit: " ;
+            cin >> amount;
+            atm.depost(amount);
+            cout <<"Depost Successfull..." << endl;
+            break;
+        case 4:
+            cout <<"Thanks for using ATM.......!" << endl;
+            break;
         default:
+            cout <<"Invalid Choice "<< endl;
             break;
         }
-    
+    cout <<"\nDo you want to try Another trasation [YES/NO]?: ";
+    cin >>choice;
     }while(choice =='y' || choice =='Y');
 
     system("pause");
